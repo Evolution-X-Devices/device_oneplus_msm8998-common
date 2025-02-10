0af8408b7ed75e681240677d882a9ca1e10b37c3
Onelots <onelots@onelots.fr>
2025-02-11 00:00:02 +0100

    msm8998-common: Setup for EvolutionX 11.x
    Signed-off-by: Onelots <onelots@onelots.fr>

--- common.mk
+++ common.mk
@@ -19,10 +19,13 @@ PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := true
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-lineage \
+    $(LOCAL_PATH)/overlay-evolution
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
+# Define mini gapps
+TARGET_USES_MINI_GAPPS := true
 # Partitions
 PRODUCT_PACKAGES += \
     vendor_bt_firmware_mountpoint \
